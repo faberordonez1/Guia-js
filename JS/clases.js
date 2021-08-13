e134f377806cb291fd6f999cd4335d9e0af02041
Faber Ordoñez <faberordonez1@gmail.com>
2021-08-12 22:13:36 -0500

    Herencia POO JS

--- JS/clases.js
+++ JS/clases.js
@@ -21,3 +21,21 @@ coche1.aumentarVelocidad();  //Modificando el valor de los  atributos del objeto
 console.log(coche1);  //Muestra el objeto por consola
 console.log(coche1.velocidad); //Muestra modelo del coche por consola
 document.write(coche1.marca);
+
+//Heredando Autobus de la clase coche
+class Autobus extends Coche {
+    constructor( marca, velocidad, modelo ) {
+        super( marca, velocidad, modelo );  //Invoca al constructor de la clase padre
+        this.altura = 6;  //Define un nuevo atributo a la clase heredada 
+    }
+
+    mostrarAltura() {
+        console.log("La altura del Autobus es " + this.altura);
+    }
+}
+ //Definiendo un objeto con la clase heredada
+ var autobus1 = new Autobus ("Mercedez",100,2019);
+
+ autobus1.aumentarVelocidad(); //Ejecuta el método heredado
+ autobus1.mostrarAltura();  //Ejecutando el nuevo método de la clase hereda
+ console.log(autobus1.velocidad);  //imprime un atributo
